3503db9605ac8c413f6f7f4705b7426a028685d4
ilovetux <me@ilovetux.com>
2016-09-09 13:51:50 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "mast.datapower.developer",
-    version = "2.1.0",
+    version = "2.2.0",
     author = "Clifford Bressette",
     author_email = "cliffordbressette@mcindi.com",
     description = ("A utility to help developers for IBM DataPower"),
